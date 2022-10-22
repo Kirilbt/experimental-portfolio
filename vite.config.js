e200f2b2e645545b2be1c6f655a21e282502cb7b
Kiril Tucker <kiril.tucker@gmail.com>
2022-10-22 17:21:35 +0200

    Commit Before Smooth Scrolling

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl({
-    defaultExtension: 'glsl',                   // Shader suffix when no extension is specified
-    compress: false,                            // Compress output shader code
-    watch: true,                                // Recompile shader on change
+    defaultExtension: 'glsl', // Shader suffix when no extension is specified
+    compress: false,          // Compress output shader code
+    watch: true,              // Recompile shader on change
     root: '/'
   })]
 });
